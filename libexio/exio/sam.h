484c90b7d711f23eb4fb5ae3fad020cc6e4e115e
Darren Smith <git@darrenjs.net>
2013-03-04 21:49:56 +0000

    added GPLv3 licence

--- libexio/exio/sam.h
+++ libexio/exio/sam.h
@@ -1,3 +1,22 @@
+/*
+    Copyright 2013, Darren Smith
+
+    This file is part of exio, a library for providing administration,
+    monitoring and alerting capabilities to an application.
+
+    exio is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    exio is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with exio.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef __SAM_HXX_
 #define __SAM_HXX_
 
