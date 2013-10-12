d3e0485207ec6784e8c4c26336ca7cfdd2ed3f23
Darren Smith <git@darrenjs.net>
2013-10-12 10:17:48 +0000

    watch for core dumps

--- test/scripts/admin_check.sh
+++ test/scripts/admin_check.sh
@@ -11,11 +11,8 @@ admin_report_dir=/var/tmp/adminlogs
 mkdir -p $admin_report_dir
 
 
-
 # Run the admin
-
 N=0
-
 while true;
 do
     echo Run $N
@@ -23,39 +20,34 @@ do
 
     $admincmd -d -d -d -d $server_addr $server_port diags  >  ${adminlog}
 
-
-
- # now check the admin completed ok
+    # now check the admin completed ok
 
     string_to_find="tables"
-
 
     nmatches=`fgrep -c -x ${string_to_find}  ${adminlog}`
     if [ $nmatches == "2" ] ;
     then
-        if [ $N != "0" ];
+        if [ $N == "0" ];
         then
-            rm -f ${adminlog}
+            cp ${adminlog} ${adminlog}.initial
         fi
     else
         echo problem with run ${N}
-        mv   ${adminlog}  ${adminlog}.problem
+        cp   ${adminlog}  ${adminlog}.problem
         break;
     fi
 
+    if [ -e core ];
+    then
+        echo core dump, run ${N}
+        mv   core core.run${N}
+        cp  ${adminlog}  ${adminlog}.coredump
+        break;
+    fi
+
+    # under normal conditions, we remove the log
+    rm -f ${adminlog}
     N=`expr $N + 1`
 
 done
-
-
-
-
-
-
-
-
-
-
-
-
 
